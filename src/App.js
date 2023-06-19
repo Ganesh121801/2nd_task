15566cc5a50225b09deac54d43d1b74c2fb645d4
Ganesh121801 <utmsg1218@gmail.com>
2023-06-19 15:26:31 +0530

    FirstProject

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Banner from "./components/Banner";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+     <Header />
+     <Banner />
+     <Contact />
+     <Footer />
+    </>
+  );
+}
+
+export default App;
